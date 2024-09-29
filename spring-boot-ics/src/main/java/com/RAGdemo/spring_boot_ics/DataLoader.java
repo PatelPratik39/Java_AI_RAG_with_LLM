eb91ef6e93bc470bba26d14bcaf0e4a793240406
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-29 13:21:08 -0500

    ready to use

--- spring-boot-ics/src/main/java/com/RAGdemo/spring_boot_ics/DataLoader.java
+++ spring-boot-ics/src/main/java/com/RAGdemo/spring_boot_ics/DataLoader.java
@@ -1,6 +1,10 @@
 package com.RAGdemo.spring_boot_ics;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
@@ -27,10 +31,23 @@ public class DataLoader {
         Integer count = jdbcClient.sql("Select COUNT(*) from vectore_store")
                 .query(Integer.class).single();
 
-        System.out.println("No of Records n the PG Vectore Store: " + count);
+        System.out.println("No of Records n the PG Vector Store: " + count);
 
+//        simple check if data is present or not
         if(count == 0){
             System.out.println("Loading Indian Constitution in pdf format from pgvectore store");
+
+//            Pdf file configuration
+            PdfDocumentReaderConfig config = PdfDocumentReaderConfig.builder()
+                    .withPagesPerDocument(1)
+                    .build();
+            PagePdfDocumentReader reader = new PagePdfDocumentReader(pdfResource,config);
+
+            var textSplitter = new TokenTextSplitter();
+            vectorStore.accept(textSplitter.apply(reader.get()));
+
+            System.out.println("Application is ready to new Requests : ");
+
         }
     }
 
