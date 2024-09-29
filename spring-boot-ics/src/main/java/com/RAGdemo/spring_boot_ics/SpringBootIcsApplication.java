a0cc5c5837fc1eed8d8907dc81d8e4c67267e900
PatelPratik39 <ptl.pratik717@gmail.com>
2024-09-29 12:46:06 -0500

    Created Basic project using Spring initializer with required Dependencies

---
+++ spring-boot-ics/src/main/java/com/RAGdemo/spring_boot_ics/SpringBootIcsApplication.java
@@ -0,0 +1,13 @@
+package com.RAGdemo.spring_boot_ics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIcsApplication.class, args);
+	}
+
+}
---
+++ spring-boot-ics/src/test/java/com/RAGdemo/spring_boot_ics/SpringBootIcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RAGdemo.spring_boot_ics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
